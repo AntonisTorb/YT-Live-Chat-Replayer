d36be2312361a3bc7c5432bc949a31630fe0571a
antonistorb <antonistorb@yahoo.com>
2023-03-23 16:23:16 +0200

    Changes:
    - Added proper time tracking to display chat.

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ A GUI app that can replay the live chat from a YouTube live stream, using the JS
 
 
 ## Need to:
-- Generate a dict with the timestamp as key, tuple 0f comments as value.
-- Add `start`/`pause` button and proper time tracking.
-- Update the Multiline element with the most recent comments up to a limit (maybe use a `collections.deque` to keep it limited to avoid `if-else` statements for length?)
+- ~~Generate a dict with the timestamp as key, tuple 0f comments as value.~~ Done.
+- ~~Add `start`/`pause` button and proper time tracking.~~ Need to resume properly
+- ~~Update the Multiline element with the most recent comments up to a limit (maybe use a `collections.deque` to keep it limited to avoid `if-else` statements for length?)~~ Done!
 - Add option to jump to timestamp, rewind or go forward maybe?
 - Figure out if possible to display images in the Multiline so I can add emotes.
 - Add display of superchat comments (`liveChatPaidMessageRenderer` instead of `liveChatTextMessageRenderer`).
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import PySimpleGUI as sg
 from datetime import datetime as dt
 import json
+from collections import deque
+
+TEXT_COLOR = "#eee3e3"
+BG_COLOR = "#202020"
 
 with open("test.json", encoding="utf8") as file:
     data = file.readlines()
@@ -8,9 +12,9 @@ with open("test.json", encoding="utf8") as file:
     for line in data:
         chat_data.append(json.loads(line))
 
-comments = [] # make this into dict
+comments = {}
 
-for line in chat_data[0:150]:
+for line in chat_data:#[2:150]:
     for action in line["replayChatItemAction"]["actions"]:
         try:
             item = action["addChatItemAction"]["item"]
@@ -20,77 +24,88 @@ for line in chat_data[0:150]:
                 pass
             try:
                 timestamp = source["timestampText"]["simpleText"]
+                if timestamp.startswith("-"):
+                    timestamp = f"-00:0{timestamp[1:]}"
+                elif len(timestamp) == 4:
+                    timestamp = f"00:0{timestamp}"
+                elif len(timestamp) == 5:
+                    timestamp = f"00:{timestamp}"
+                elif len(timestamp) == 7:
+                    timestamp = f"0{timestamp}"
                 author = source["authorName"]["simpleText"]
                 message = ""
                 message_list = source["message"]["runs"]
                 for message_dict in message_list:
-
                     for message_type, message_segment in message_dict.items():
                         if message_type == "emoji":
-                            try:
+                            try: # custom emote
                                 add_txt = message_segment["shortcuts"][1]
                             except IndexError:
                                 add_txt = message_segment["shortcuts"][0]
                             message = f"{message}{add_txt} "
                         elif message_type == "text":
                             message = f"{message}{message_segment} "
-                    
-                comments.append((timestamp, author, message))
+                if timestamp in comments.keys():
+                    comments[timestamp].append((timestamp, author, message))
+                else:
+                    comments[timestamp] = [(timestamp, author, message)]
             except NameError:
                 pass
-
         except KeyError:
             pass
 
-new_bg = False
+#print(comments)
 
-def get_bg(new_bg):
-    if new_bg:
-        return "#303030", False
-    else:
-        return "#202020", True
-
-bg_col, new_bg = get_bg(new_bg)
-
+playing = False
+comment_deque =  deque("", maxlen=30)
 
 layout = [
     [sg.Text("Chat Replay")],
-    [sg.Multiline("", expand_x=True, expand_y=True, background_color=bg_col, 
-                  text_color= "#eee3e3", autoscroll=True, write_only=True, disabled=True, reroute_stdout=True, key="-CHAT-")],
-    [sg.Button("Exit"), sg.Button("Print")]
+    [sg.Multiline("", expand_x=True, expand_y=True, background_color=BG_COLOR, 
+                  text_color= TEXT_COLOR, autoscroll=True, write_only=True, disabled=True, key="-CHAT-")],
+    [sg.Button("Play", key="-PLAY_PAUSE-"), sg.Push(), sg.Button("Exit")]
 ]
 
-window = sg.Window("Youtube Live Chat Replay", layout, size=(480, 640), font=("Arial", 14), 
+window = sg.Window("Youtube Live Chat Replay", layout, size=(480, 640), font=("Arial", 12), 
                    resizable=True, enable_close_attempted_event=True, finalize=True)
 #window.set_min_size((480, 640))
 
-for comment in comments:
-    bg_col, new_bg = get_bg(new_bg) 
-    #window["-CHAT-"].print(comment, colors=("#eee3e3",bg_col), autoscroll=True)
-
+def get_chat(comments, comment_deque, delta_t):
+    try:
+        for comment in comments[delta_t]:
+            if comment not in comment_deque:
+                comment_deque.append(comment)
+        
+        #print(comment_deque)
+        return comment_deque
+    except KeyError:
+        return comment_deque
 
 while True:
-    event, values = window.read(timeout=100)
-    cur_time = dt.now().strftime("%H:%M:%S")
-    #print(cur_time)
+    event, values = window.read(timeout=250)
+    cur_time = dt.now()
+    if playing:
+        delta_t = str(cur_time - start_time).split(".", 2)[0]
+        if len(delta_t) == 7:
+            delta_t = f"0{delta_t}"
+        comment_deque = get_chat(comments, comment_deque, delta_t)
+        if comment_deque:
+            window["-CHAT-"].update("")
+            for comment in comment_deque:
+                window["-CHAT-"].update(f"{comment[0]:<12} ", append=True, text_color_for_value="red", background_color_for_value=BG_COLOR)
+                window["-CHAT-"].update(f"{comment[1]}\n", append=True, text_color_for_value="green", background_color_for_value=BG_COLOR)
+                window["-CHAT-"].update(f"{comment[2]}\n\n", append=True, text_color_for_value=TEXT_COLOR, background_color_for_value=BG_COLOR)
+
     match event:
         case sg.WIN_CLOSE_ATTEMPTED_EVENT | "Exit":
             break
-        case "Print":
-            for comment in comments:
-                bg_col, new_bg = get_bg(new_bg)
-                if comment[0].startswith("-"):
-                    timestamp = f"-00:0{comment[0][1:]}"
-                elif len(comment[0]) == 4:
-                    timestamp = f"00:0{comment[0]}"
-                elif len(comment[0]) == 5:
-                    timestamp = f"00:{comment[0]}"
-                elif len(comment[0]) == 7:
-                    timestamp = f"0{comment[0]}"
-                else:
-                    timestamp = comment[0]
-                window["-CHAT-"].update(f"{timestamp:<12} ", append=True, text_color_for_value="red", background_color_for_value=bg_col)
-                window["-CHAT-"].update(f"{comment[1]:<30} ", append=True, text_color_for_value="green", background_color_for_value=bg_col)
-                window["-CHAT-"].update(f"{comment[2]} ", append=True, text_color_for_value="white", background_color_for_value=bg_col)
-                window["-CHAT-"].update("\n", append=True, text_color_for_value="red", background_color_for_value=bg_col)
+        case "-PLAY_PAUSE-":
+            if not playing:
+                playing = True
+                window["-PLAY_PAUSE-"].update("Pause")
+                start_time = dt.now()
+            elif playing:
+                playing = False
+                window["-PLAY_PAUSE-"].update("Resume")
+                # add pause logic and time tracking to resume
 window.close()
