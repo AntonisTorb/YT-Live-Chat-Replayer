9f7578fcf9a495cdd1e40e9d727bb81e539e1340
antonistorb <antonistorb@yahoo.com>
2023-04-02 17:03:07 +0300

    Changes:
    - Added emote fetching from web (svg does not work with PIL though).
    
    - Added support for superchat display.
    
    - Added option for semi-transparency for the window by lowering opacity (Note: this also affects the text and emotes).
    
    - Code restructuring and refactoring.

--- README.md
+++ README.md
@@ -10,10 +10,10 @@ A GUI app that can replay the live chat from a YouTube live stream, using the JS
 - ~~Add `start`/`pause` button and proper time tracking.~~ Done.
 - ~~Update the Multiline element with the most recent comments up to a limit (maybe use a `collections.deque` to keep it limited to avoid `if-else` statements for length?)~~ Done!
 - Add option to jump to timestamp, rewind or go forward maybe?
-- ~~Figure out if possible to display images in the Multiline so I can add emotes.~~ Done from local file, need to add functionality to add from URL so all the emotes work.
-- Add display of superchat comments (`liveChatPaidMessageRenderer` instead of `liveChatTextMessageRenderer`).
-- ~~Membership notifications/messages too maybe?~~ WIP, needs testing.
-    - (idea: use `message_type` to specify if superchat or membership and add different background.)
-- File selector to specify the JSON file path.
+- ~~Figure out if possible to display images in the Multiline so I can add emotes.~~ Done, need to add option to get from local file after adding the file selector.
+- ~~Add display of superchat comments (`liveChatPaidMessageRenderer` instead of `liveChatTextMessageRenderer`).~~ Done!
+- ~~Membership notifications/messages too maybe?~~ Done!
+- File selector to specify the JSON file path and emote file path (optional).
 - Option to have a different background/text color for comments from specific usernames (for example the streamer or a live translator).
+- When starting the app, after selecting the filepaths and before playback, display all the chats with a negative timestamp.
 - Properly refactor code for easier modification and performance (less loops would be nice, but the yt JSON is a bit messy).
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from collections import deque
 from datetime import datetime as dt
+from datetime import timedelta
 import json
 from pathlib import Path
 import threading
@@ -7,24 +8,63 @@ import tkinter as tk
 
 from PIL import Image
 from PIL.ImageTk import PhotoImage
+from PIL import UnidentifiedImageError
 from pynput.keyboard import Key, Listener
 import PySimpleGUI as sg
+import requests
+
+
+'''
+For our most beloved kettle <3                                                                              
+                                                     .#%%%(///#%%%*             
+                                                   #%(            /%%           
+                                                 ,%#                (%/         
+                          ,*********/  ////////  %%      %%(/%%,     #%.        
+                       ,#(        /%, (%,    %% .%#     (%*  .%#     /%,        
+                       ##        .%/ ,%/     /%, #%.      #%%#.      %%         
+                      /#.        ##  %#       #%. (%(              *%%          
+                     ,#*        (#. #%         *%#  /%%/        *%%#            
+                     #(        *#* /#,           /%#  %% *%%%%#/.               
+                    (#.       .#( .#/           .%#  %# *%/                     
+                   ,(*        (#  ##            #%%%%# *%%%%/                   
+                   ((        /#. /#.                        #%                  
+                  ((        ,#* ,#*           ##,,,,,,,,,,,,,.                  
+                 *(,        ((  #(           (######%%%%%%%(                    
+                .(/        /(  ((                      *##                      
+                /(        ,(, *(.                    .##                        
+               *(.        (/ .(((((((((###############.                         
+              .(*        ((                                                     
+              //        ................,.,,,,,,(#*                             
+               *(////((((((((((((((((((((((.  *#/                               
+                                        ,(* ,((                                 
+                                       .(/.((                                   
+                                       ((((.                                    
+                                      /((,                                      
+                                     *(*                                        
+                                    ,/                                          
+                                                                               
+'''
 
 
 FILENAME = "test.json"
 FONT_NAME = "Arial"
 FONT_SIZE = 12
+STYLE = "bold" # For no style use empty string: ""
 TEXT_COLOR = "#eee3e3"
 TIMESTAMP_COLOR = "red"
 USERNAME_COLOR = "orange"
 BG_COLOR = "#202020"
 MEMBER_BG_COLOR = "blue"
+SUPERCHAT_BG_COLOR = "green"
 EMOTE_SIZE = (24, 24)
+CHAT_LENGTH = 30 # Reducing this increadses perfiormance.
 PLAY_PAUSE_KEY = Key.space
 DISABLE_TITLEBAR = True # If True, then no window resizing after running the app.
 WINDOW_SIZE = (480, 640) # If 'DISABLE_TITLEBAR' is True, set the window size (width, height)
+TRANSPARENT = False
+ALPHA = 0.7 # Change to 1 or None if TRANSPARENT is True.
 
-key_event = False
+key_event = False # Global var used to track if the Play/Pause/Resume key was pressed, since GUI and event listener are in different threads.
 
 
 def on_press(key:Key) -> None:
@@ -35,8 +75,8 @@ def on_press(key:Key) -> None:
         key_event = False if key_event else True
 
 
-def listen_kb():
-    '''Event listener for keyboard events.'''
+def listen_kb() -> None:
+    '''Event listener for keyboard events. Needs to be in a separate thread to the GUI, so they can coexist.'''
 
     with Listener(on_press=on_press) as listener:
         listener.join()
@@ -56,72 +96,102 @@ def format_timestamp(timestamp:str) -> str:
     return timestamp
 
 
-def get_comments() -> dict[str, list[tuple[str,str,tuple[str,str]]]]:
+def populate_emote_links(emote_txt:str, emote_link_dict:dict[str,str], url:str) -> None:
+    '''Populates a dictionary with the emote name as key and emote link as value for every unique emote.
+    Svg images not supported by Pillow, so we will be using the emoji instead.
+    '''
+
+    if emote_txt not in emote_link_dict.keys() and not url.endswith("svg"):
+        emote_link_dict[emote_txt] = url
+
+
+def get_comments(filepath:str|Path, emote_link_dict:dict[str,str]) -> dict[str, list[tuple[str,str,tuple[str,str]]]]:
     '''Get a dictionary of all comments with the timestamp as key, and a list of tuples as value.
     The values contain the timestamp, author of the message and the message as a tuple.
     The message tuple contains the message type (text, emoji, etc.) and the message content.
     '''
 
-    with open(FILENAME, encoding="utf8") as file:
+    with open(filepath, encoding="utf8") as file:
         data = file.readlines()
         chat_data = []
         for line in data:
             chat_data.append(json.loads(line))
 
     comments = {}
-    for line in chat_data[0:1000]:
+    for line in chat_data:#[1780:1810]:
         for action in line["replayChatItemAction"]["actions"]:
             if "addLiveChatTickerItemAction" in action:
-                continue # membership stuff
+                continue
             item = action["addChatItemAction"]["item"]
-
-            if "liveChatTextMessageRenderer" in item:
+            amount = None
+            if "liveChatTextMessageRenderer" in item: # Normal chat.
                 source = item["liveChatTextMessageRenderer"]
                 message_list = source["message"]["runs"]
-            elif "liveChatMembershipItemRenderer" in item:
+                timestamp = format_timestamp(source["timestampText"]["simpleText"])
+                author = source["authorName"]["simpleText"]
+            elif "liveChatMembershipItemRenderer" in item: # Member.
                 source = item["liveChatMembershipItemRenderer"]
                 if "headerPrimaryText" in source:
                     message_list = source["headerPrimaryText"]["runs"]
                     if "message" in source:
-                        message_list_2 = source["message"]["runs"]
+                        member_message_list = source["message"]["runs"]
                 else:
                     message_list = source["headerSubtext"]["runs"]
+                timestamp = format_timestamp(source["timestampText"]["simpleText"])
+                author = source["authorName"]["simpleText"]
+            elif "liveChatPaidMessageRenderer" in item: # Superchat.
+                source = item["liveChatPaidMessageRenderer"]
+                amount = source["purchaseAmountText"]["simpleText"]
+                timestamp = format_timestamp(source["timestampText"]["simpleText"])
+                author = source["authorName"]["simpleText"]
+                try:
+                    message_list = source["message"]["runs"]
+                except KeyError:
+                    message_list = []
             else:
                 continue
-            
-            timestamp = format_timestamp(source["timestampText"]["simpleText"])
-            
-            author = source["authorName"]["simpleText"]
-            
-            message = []
-            
+
+            if amount:
+                message = [("superchat", amount)]
+            else:
+                message = []
+
             for message_dict in message_list:
                 for message_type, message_segment in message_dict.items():
                     if message_type == "emoji":
-                        try: # custom emote
-                            emote_txt = message_segment["searchTerms"][1]
-                        except IndexError:
-                            emote_txt = message_segment["searchTerms"][0]
-                        except KeyError:
-                            pass # see line 65275 in formattinghelp.json, annoying, no search term for handwave?!
+                        if len(message_segment["emojiId"]) <= 2:
+                            emote_txt = message_segment["emojiId"]
+                            populate_emote_links(emote_txt, emote_link_dict, message_segment["image"]["thumbnails"][0]["url"])
+                        else:
+                            try: # custom emote
+                                emote_txt = message_segment["searchTerms"][1]
+                                populate_emote_links(emote_txt, emote_link_dict, message_segment["image"]["thumbnails"][0]["url"])
+                            except IndexError:
+                                emote_txt = message_segment["searchTerms"][0]
+                                populate_emote_links(emote_txt, emote_link_dict, message_segment["image"]["thumbnails"][0]["url"])
                         message.append((message_type, emote_txt))
                     elif "liveChatMembershipItemRenderer" in item and message_type == "text":
                         message.append(("membership", message_segment))
                     elif message_type == "text":
                         message.append((message_type, message_segment))
             try: # For membership messages.
-                for message_dict in message_list_2:
+                for message_dict in member_message_list:
                     for message_type, message_segment in message_dict.items():
                         if message_type == "emoji":
-                            try: # custom emote
-                                emote_txt = message_segment["searchTerms"][1]
-                            except IndexError:
-                                emote_txt = message_segment["searchTerms"][0]
-                            except KeyError:
-                                pass # see line 65275 in formattinghelp.json, annoying, no search term for handwave?!
+                            if len(message_segment["emojiId"]) <= 2:
+                                emote_txt = message_segment["emojiId"]
+                                populate_emote_links(emote_txt, emote_link_dict, message_segment["image"]["thumbnails"][0]["url"])
+                            else:
+                                try: # custom emote
+                                    emote_txt = message_segment["searchTerms"][1]
+                                    populate_emote_links(emote_txt, emote_link_dict, message_segment["image"]["thumbnails"][0]["url"])
+                                except IndexError:
+                                    emote_txt = message_segment["searchTerms"][0]
+                                    populate_emote_links(emote_txt, emote_link_dict, message_segment["image"]["thumbnails"][0]["url"])
                             message.append((message_type, emote_txt))
                         elif message_type == "text":
                             message.append((message_type, message_segment))
+                member_message_list = []
             except UnboundLocalError:
                 pass
             
@@ -129,6 +199,7 @@ def get_comments() -> dict[str, list[tuple[str,str,tuple[str,str]]]]:
                 comments[timestamp].append((timestamp, author, message))
             else:
                 comments[timestamp] = [(timestamp, author, message)]
+
     return comments
 
 
@@ -144,19 +215,41 @@ def get_chat(comments:dict[str, list[tuple[str,str,tuple[str,str]]]],
             comment_deque.append(comment)
     return comment_deque
 
+def prepare_emotes(emote_link_dict:dict[str,str]) -> dict[str,PhotoImage]:
+    '''Produces a dictionary with the emote name as key and a PhotoImage object containing 
+    the emote image so it can be displayed in the chat window.
+    '''
+
+    emote_images_pil = {}
+    for emote_name, link in emote_link_dict.items():
+        if link.endswith(".svg"):
+            continue
+        try:
+            emote_images_pil[emote_name] = Image.open(requests.get(link, stream=True).raw).resize(EMOTE_SIZE)
+        except UnidentifiedImageError:
+            pass
+
+    # emote_images_pil = {filepath.name[:-4]: Image.open(str(filepath)).resize(EMOTE_SIZE) for filepath in emote_list}
+    
+    # for filepath in emote_list:
+    #     emote_images_pil[filepath.name[:-4]] = Image.open(str(filepath)).resize(EMOTE_SIZE)
+
+    return {name: PhotoImage(image=image) for name, image in emote_images_pil.items()}
+
 
 def main_window() -> None:
     '''Main window construction and logic loop.'''
     
     playing = False
-    paused_timestamp = None
+    paused_timestamp = None #timedelta(hours=2, minutes=14,seconds=50)
+    emote_link_dict = {}
+    comments = get_comments(FILENAME, emote_link_dict)
 
-    comments = get_comments()
-    comment_deque = deque("", maxlen=30)
+    comment_deque = deque("", maxlen=CHAT_LENGTH)
 
     emote_list = list(Path("emotes").glob("*.png"))
     emote_names = [emote.name[:-4] for emote in emote_list]
-    # For our most beloved kettle <3
+    
     class Multiline(sg.Multiline):
         '''Subclass so we can define the method necessary to display emotes in the Multiline element.'''
 
@@ -173,18 +266,30 @@ def main_window() -> None:
         [sg.Button("Play", key="-PLAY_PAUSE-"), sg.Push(), sg.Button("Exit")]
     ]
 
-    window = sg.Window("Youtube Live Chat Replay", layout, size=(480,640), font=(FONT_NAME, FONT_SIZE), 
+    transparent_color = BG_COLOR if TRANSPARENT else None
+
+    window = sg.Window("Youtube Live Chat Replay", layout, size=(480,640), font=(FONT_NAME, FONT_SIZE, STYLE), 
                     resizable=True, enable_close_attempted_event=True, sbar_trough_color=BG_COLOR,
                     no_titlebar=DISABLE_TITLEBAR, grab_anywhere=True, keep_on_top=True, background_color=BG_COLOR,
-                    transparent_color=BG_COLOR, margins=(0,0), element_padding=(0,0), finalize=True)
+                    transparent_color=transparent_color, margins=(0,0), element_padding=(0,0), alpha_channel=ALPHA,finalize=True)
     #window.set_min_size((480, 640))
     if DISABLE_TITLEBAR:
         window.size = WINDOW_SIZE
     chat_window = window["-CHAT-"]
+    
+    chat_window.update("Getting emotes from web, please wait...\n", 
+                                    append=True, 
+                                    text_color_for_value=TIMESTAMP_COLOR, 
+                                    background_color_for_value=BG_COLOR)
+    window.refresh()
 
-    emote_images_pil = {filepath.name[:-4]: Image.open(str(filepath)).resize(EMOTE_SIZE) for filepath in emote_list}
-    emote_images = {name: PhotoImage(image=image) for name, image in emote_images_pil.items()}
-
+    emote_images = prepare_emotes(emote_link_dict)
+    
+    chat_window.update("Ready to play!\n", 
+                                    append=True, 
+                                    text_color_for_value=TIMESTAMP_COLOR, 
+                                    background_color_for_value=BG_COLOR)
+    
     while True:
 
         global key_event
@@ -211,7 +316,13 @@ def main_window() -> None:
             if comment_deque:
                 chat_window.update("")
                 for comment in comment_deque:
-                    bg_color = MEMBER_BG_COLOR if "membership" in comment[2][0] else BG_COLOR
+                    if "membership" not in comment[2][0] and "superchat" not in comment[2][0]:
+                        bg_color = BG_COLOR
+                    elif "membership" in comment[2][0]:
+                        bg_color = MEMBER_BG_COLOR
+                    elif "superchat" in comment[2][0]:
+                        bg_color = SUPERCHAT_BG_COLOR
+
                     chat_window.update(f"{comment[0]:<12} ", 
                                     append=True, 
                                     text_color_for_value=TIMESTAMP_COLOR, 
@@ -228,20 +339,23 @@ def main_window() -> None:
                             )
                         elif message_type == "emoji":
                             emote_img = message_content
-                            if emote_img in emote_names:
+                            if emote_img in emote_images.keys():
                                 _ = chat_window.image_create(tk.INSERT, emote_img, padx=2)
                             else:
-                                chat_window.update(f":{message_content}: ", 
+                                chat_window.update(f"{message_content} ", 
                                                 append=True, 
                                                 text_color_for_value=TEXT_COLOR, 
                                                 background_color_for_value=bg_color)
-                        elif message_type == "membership":
+                        elif message_type == "membership" or message_type == "superchat":
                             chat_window.update(
                                 f"{message_content} ", append=True, text_color_for_value=TEXT_COLOR, 
                                 background_color_for_value=bg_color
                             )
-                    chat_window.update("\n\n", append=True, 
-                                    text_color_for_value="red", background_color_for_value=bg_color)
+
+                    chat_window.update("\n", append=True, 
+                                    text_color_for_value=TEXT_COLOR, background_color_for_value=bg_color)
+                    chat_window.update("\n", append=True, 
+                                    text_color_for_value=TEXT_COLOR, background_color_for_value=BG_COLOR)
 
         match event:
             case sg.WIN_CLOSE_ATTEMPTED_EVENT | "Exit":
@@ -257,8 +371,10 @@ def main_window() -> None:
                     paused_timestamp = chat_timestamp_dt
     window.close()
 
+def main():
+    threading.Thread(target=main_window).start()
+    threading.Thread(target=listen_kb, daemon=True).start() 
 
 if __name__ == "__main__":
     
-    threading.Thread(target=main_window).start()
-    threading.Thread(target=listen_kb, daemon=True).start()
+    main()
